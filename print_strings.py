3ad2cf3914167ea8888d3b26485cd00e69f02ee6
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 02:43:30 +0200

    Add files via upload

---
+++ print_strings.py
@@ -0,0 +1,12 @@
+#This program demonstrates how to print two strings to the console. 
+#It shows how text can be displayed as output and how multiple strings 
+#can be printed in a single program.
+
+#declare first string and ask for user input
+first_name= input("What is the first name? ")
+
+#declare second string and ask for user input
+second_name= input("What is the second name? ")
+
+#display the message containing both strings
+print("Good morning ", first_name,"!!! Good morning ", second_name)
