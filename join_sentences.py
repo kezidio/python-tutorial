2fced9749e1a807693d69294257f65942860072d
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 13:48:32 +0200

    Update join_sentences.py

--- join_sentences.py
+++ join_sentences.py
@@ -8,5 +8,8 @@ def join_sentence(list_of_sentences, index):
     sentence = " ".join(list_of_sentences[index])
     return sentence
 
-sentence = join_sentence(list_of_sentences, 1)
-print(sentence)
+first_sentence = join_sentence(list_of_sentences, 0)
+print(first_sentence)
+
+second_sentence = join_sentence(list_of_sentences, 1)
+print(second_sentence)
