dd5abc10ace8b8fa219bfc85c4da68ec3577d466
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 06:00:39 +0200

    Refactor final_grade.py for clarity and structure
    Refactor final_grade.py to improve readability and structure.

--- final_grade.py
+++ final_grade.py
@@ -2,15 +2,20 @@
 #calculates the final grade usinf a while loop
 # Author: Kaweny Ezidio
 
-
+#create variables to store data
 final_grade: 0
+
+#ask user to enter input
 m = int(input("How many assignments did you complete? "))
+#make a copy of the number of assignments
 n = m
+#declare variable to sum all grades
 grades_sum = 0
+#create a while loop so user can enter all grades
 while n!= 0:
 	grade =float(input("Enter grade: "))
-	grades_sum = grades_sum + grade
-	n=n-1
-
+	grades_sum = grades_sum + grade #sum all grades
+	n=n-1 #count down the number of iterations
+#calculate final grade and display it
 final_grade = grades_sum/m
 print("Your final grade is :",final_grade)
