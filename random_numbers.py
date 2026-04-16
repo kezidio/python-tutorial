50c382b09a91bfefadfdc9fde7672ff1d25c8e8d
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 13:28:15 +0200

    Add files via upload

---
+++ random_numbers.py
@@ -0,0 +1,11 @@
+
+
+import random
+def main():
+    # Get a random number.
+    number = random.randint(1, 10)
+    # Display the number.
+    print('The number is', number)
+
+# Call the main function.
+main()
